05d1f4dd8902592941c0f771d4277a539b3c720f
erikweis <32458904+erikweis@users.noreply.github.com>
2022-01-05 12:38:01 -0500

    modify examples

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ import streamlit as st
 import networkx as nx
 import numpy as np
 import json
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import ast
 
 import random
 
@@ -67,16 +70,19 @@ def callback_avm():
 
 st.title("Streamlit Force Graph Simulator Demo")
 
-st.markdown("""Creating network visualizations from python can be tricky with existing tools. Fortunately,
-javascript libraries like d3 have very good network visualization tools, such as force-directed-layouts.
-However, these libraries are inaccessible to those who don't regularly deal with front-end code. The streamlit-force-graph-simulator
-component is designed to allow making force-directed visualizations easy from python.
+st.markdown("""The D3 javascript package is amazing for creating dynamic force-directed graph layouts. Unfortunately,
+doing so is difficult without knowledge of javascript, and these graphs aren't easy to generate from python. Fortunately,
+streamlit handles the connection between python and javascript automatically, and the `streamlit-force-graph-simulator`
+component makes it easy to create force-directed visualizations (and simulations) from python.
 """.replace("\n",' '))
 
-st.markdown('Documentation can be found at [here](https://github.com/erikweis/streamlit-force-graph-simulator).')
+st.markdown("""Documentation can be found at [here](https://github.com/erikweis/streamlit-force-graph-simulator). Code for this
+demo is available from the menu bar in the top right.""".replace("\n",' '))
+
+st.header("Example Simulations")
 
 ######## Adaptive Voter Model
-st.header("Adaptive Voter Model")
+st.subheader("Adaptive Voter Model")
 
 st.markdown("""
 With the traditional voter model, a random node takes the state of one of its neighbors at each time step. With the adaptive voter model
@@ -116,20 +122,26 @@ st_graph(
 
 ##### Preferential attachment
 
-st.header("Preferential Attachement")
+st.subheader("Random Walk Model")
 
 
 st.markdown("""
-With preferential attachement, nodes are added to the network one at a time. And each are 
-connected to $n$ new nodes.
+The random walk model can be thought of as an extension of preferential attachment model with higher clustering.
+At each time step, a node is added to the network and attached to a randomly chosen node $j$. Then, the new node's friends
+are also created by adding links to each neighbor of $j$ with probability $p$. Implicitly,
+this process induces preferential attachment, because nodes with high degree are more likely to be
+connected to the randomly chosen node $j$.
 """.replace("\n",''))
 
+col1,col2 = st.columns((2,3))
 
-num_seed_nodes = 5
-st.slider("Number of Attachement Nodes",min_value=1,max_value=num_seed_nodes,key='num_seed_nodes')
+with col1:
+    st.write('Controls')
+    st.slider("Probability of Attaching to Friends of $j$",min_value=0.0,max_value=1.0,value=0.8,key='prob_link_rwm')
+    st.slider("Number of Initial Friends",min_value=2,max_value=6,key='num_initial_friends')
 
 #seed graph
-
+num_seed_nodes = 5
 G = nx.complete_graph(num_seed_nodes)
 F_pa = ForceGraphSimulation(G)
 
@@ -137,28 +149,54 @@ F_pa = ForceGraphSimulation(G)
 for i in range(num_seed_nodes,100+num_seed_nodes):
 
     G = F_pa.graph
-    
-    #random choice according to degree
-    nodes, degrees = map(lambda x: np.array(x),zip(*list(G.degree())))
-    attachment_nodes = np.random.choice(
-        nodes,
-        p=degrees/np.sum(degrees),
-        size=st.session_state.num_seed_nodes
-    )
 
     # add node to network
+    edge_color = 'rgba(220,220,220,0.4)'
+
+    i = len(G.nodes)
+    j = random.choice(list(G.nodes))
     F_pa.add_node(i)
-    for j in attachment_nodes:
-        F_pa.add_edge(i, int(j))
+    F_pa.add_edge(i,j,color=edge_color)
+    for neighbor in G[j]:
+        if random.random() < st.session_state.prob_link_rwm:
+            F_pa.add_edge(i,random.choice(list(G[j])),color=edge_color)
+        else:
+            F_pa.add_edge(i,random.choice(list(G.nodes)),color=edge_color)
+    
+    # set node color
+    degrees = list(G.degree)
+    max_deg = max(degrees,key=lambda x:x[1])[1]
+    cmap = cm.get_cmap('viridis',50)
+
+    for node, degree in degrees:
+        
+        color = list(cmap(degree/max_deg))
+        color = [int(np.floor(256*c)) if i < 3 else c for i,c in enumerate(color)]
+
+        F_pa.set_node_attributes(node,color='rgba({},{},{},{})'.format(*color))
+
     F_pa.save_event()
 
+
+with col2:
+    degrees = [item[1] for item in F_pa.graph.degree()]
+    fig, ax = plt.subplots()
+    ax.hist(degrees,bins='auto')
+    ax.set_title('Degree Distribution')
+    st.pyplot(fig)
+
+
 st_graph(
-    F_pa.initial_graph_json, 
-    F_pa.events,
-    time_interval=500,
-    graphprops = graphprops,
-    key="graph_pa"
-)
+        F_pa.initial_graph_json, 
+        F_pa.events,
+        time_interval=100,
+        graphprops = {
+            'height':500,
+            'linkColor':'color',
+            'nodeColor':'color'
+        },
+        key="graph_pa"
+    )
 
 ######### Static Graphs ###########
 
@@ -168,7 +206,8 @@ st.markdown("""We can also use the streamlit force graph component to visualize 
 This can be done by not passing an events list to the component.""".replace("\n"," "))
 
 st.markdown("""This graph shows trust ratings between users on Bitcoin OTC. 
-The edges are signed and weighted.""".replace("\n",' '))
+The edges are signed and weighted, so positive trust ratings are shown in blue, while negative trust ratings are 
+shown in red. Edges are also weighted according to the strength of the rating, which took a value between $[-10,10]$.""".replace("\n",' '))
 
 with open('bitcoinc.json','r') as f:
     jg = json.load(f)
@@ -177,15 +216,51 @@ st_graph(
     jg,
     graphprops = {
         'height':600,
-        'linkWidth':'width',
-        #'linkColor':'color'
+        'linkWidth':'linkWidth',
+        'linkColor':'color',
+        'nodeRelSize':6,
         },
-    view='3D',
     key='graph_static'
 )
 
+###### Stochastic Block Model ##################
 
-##############
+st.header('Stochastic Block Model')
+
+def callback_sbm():
+
+    try:
+        sizes = ast.literal_eval(st.session_state.sbm_sizes)
+        matrix = ast.literal_eval(st.session_state.connection_matrix)
+        sbm_nx = nx.stochastic_block_model(sizes,matrix)
+        sbm = nx.readwrite.json_graph.node_link_data(sbm_nx)
+        sbm['graph'].pop('partition')
+        st.session_state.sbm_graph = sbm
+    except:
+        pass
+
+
+sizes = [25,25,25]
+matrix =   [[0.5, 0.05, 0.05], [0.05, 0.5, 0.05], [0.05, 0.05, 0.5]]
+sbm_nx = nx.stochastic_block_model(sizes,matrix)
+sbm = nx.readwrite.json_graph.node_link_data(sbm_nx)
+
+# label
+sbm['graph'].pop('partition') #excluse partition data because it's not json serializable and not useful
+
+if 'sbm_graph' not in st.session_state:
+    st.session_state.sbm_graph = sbm
+
+st.text_input('Sizes',sizes,key='sbm_sizes',on_change=callback_sbm)
+st.text_area('Edge Density Matrix Matrix',str(matrix).replace('],','],\n'),key='connection_matrix',on_change=callback_sbm)
+
+st_graph(
+    st.session_state.sbm_graph,
+    graphprops = {'height':500,'nodeAutoColorBy':'block'},
+    key='sbm_graph'
+)
+
+############## Continuous Play ################
 
 st.header('Continuous Play')
 
